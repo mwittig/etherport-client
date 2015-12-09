86c3d0119607d771fd41b4453552f859ee381674
marcus <WittigMarcus@gmail.com>
2015-12-09 19:28:33 +0100

    Complete rewrite. Added chaining feature. Added documentation.

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
 # etherport-client
 
 Client-side virtual serial port for Rick Waldron's [Etherport](https://github.com/rwaldron/etherport). 
-Etherport-client is used to implement firmata-compatible boards and relays which can be controlled by a remote entity. 
+Etherport-client is used to implement firmata-compatible boards and tethering hubs to control a board 
+by a remote entity.
+ 
+# Usage Examples
+
+## Tethering a Johnny Five IO-Plugin
+ 
+Create an `EtherPortClient` with `host` and `port` properties referring to the peer transport endpoint of the
+`EtherPort` instance. Next, bind the resulting object to an IO-plugin using 
+[remote-io](https://github.com/monteslu/remote-io) (see example below).
+
+     var EtherPortClient = require('etherport-client').EtherPortClient;
+     var RemoteIO = require('remote-io');
+     var RaspiIO = require('raspi-io');
+     var io = new RaspiIO();
+     
+     io.on('ready', function() {
+         console.log("ready");
+     
+         var sp = new EtherPortClient({
+             host: '192.168.178.70',
+             port: 41234
+         });
+     
+     
+         //listen for remote firmata messages
+         var remoteIO = new RemoteIO({
+             serial: sp,
+             io: io
+         });
+     });
+
+ 
+ ## Tethering a Serial Port (Chaining)
+ 
+ You can also simply chain Serial Port objects. For example, if you have a host with an Arduino board connected via 
+ USB and you want to access the Arduino board from a remote computer, you chain the Serial Ports on the host as follows.
+ 
+     var EtherPortClient = require('etherport-client').EtherPortClient;
+     var chainSerialPorts = require('etherport-client').chainSerialPorts;
+     var SerialPort = require("serialport").SerialPort;
+     
+     chainSerialPorts(
+         new EtherPortClient({
+             host: '192.168.178.70',
+             port: 41234
+         }),
+         new SerialPort("/dev/ttyUSB1", {
+             baudrate: 57600
+         })
+     );
+     
+ # Debugging 
+ 
+ For debugging purposes the [debug package](https://github.com/visionmedia/debug) with topic "etherport-client" is used.
+ 
+ # License
+ 
+ [The MIT License (MIT), Copyright (c) 2015 Marcus Wittig](https://github.com/mwittig/etherport-client/blob/master/LICENSE)
+
+    
+ 
+ 
+ 
+ 
--- index.js
+++ index.js
@@ -1,60 +1,217 @@
 'use strict';
 
-var net = require("net"),
-    util = require("util"),
-    events = require("events");
+var net = require('net'),
+    util = require('util'),
+    events = require('events'),
+    debug = require('debug')('etherport-client');
+
 
 function EtherPortClient(opts) {
     events.EventEmitter.call(this);
 
-    if (typeof opts === "undefined" || typeof opts.host === "undefined" || typeof opts.port === "undefined") {
-        throw new Error("Expected object with 'host' and 'port' settings");
+    if (typeof opts === 'undefined' || typeof opts.host === 'undefined' || typeof opts.port === 'undefined') {
+        throw new Error('Expected object with "host" and "port" settings');
     }
-    this.path = "Connecting to host:port: " + opts.host + ':' + opts.port;
-    this.name = "EtherPortClient";
+    this.path = 'Connecting to host:port: ' + opts.host + ':' + opts.port;
+    this.name = 'EtherPortClient';
+    this.host = opts.host;
+    this.port = opts.port;
 
-    var self = this;
-    this.state = {
-        queue: [],
-        socket: null,
-        flushTo: function(socket) {
-            if (this.socket === null) {
-                this.socket = socket;
-                self.emit("open");
-            }
-            if (this.queue.length) {
-                this.queue.forEach(function(buffer) {
-                    self.socket.write(buffer);
-                }, this);
+    this._reconnectTimeoutSecs = opts.reconnectTimeout || 15;
+    this._queue = [];
+    this._socket = null;
+    this._reconnectTimer = null;
 
-                this.queue.length = 0;
-            }
-        }
-    };
-
-    var tcp = new net.Socket();
-    tcp.setNoDelay(true);
-    tcp.connect(opts.port, opts.host, function() {
-        console.log('XXX Connected');
-        self.state.flushTo(tcp);
-        tcp.on("data", function(data) {
-            console.log('XXX Data', data.length);
-            self.emit("data", data);
-        });
-    });
+    this._tcp = new net.Socket();
+    this._tcp.on('connect', this._onConnectHandler());
+    this._tcp.on('data', this._onDataHandler());
+    this._tcp.on('error', this._onErrorHandler());
+    this._tcp.on('timeout', this._onTimeoutHandler());
+    this._tcp.on('close', this._onCloseHandler());
+    this._connect();
 }
 util.inherits(EtherPortClient, events.EventEmitter);
 
+EtherPortClient.prototype._connect = function () {
+    debug(this.path);
+    this._tcp.setNoDelay(true);
+    this._tcp.setTimeout(15000);
+    this._tcp.connect(this.port, this.host);
+};
 
-EtherPortClient.prototype.write = function(buffer) {
+EtherPortClient.prototype._reconnect = function () {
+    var self = this;
 
-    if (this.state.socket === null) {
-        this.state.queue.push(buffer);
-    } 
-    else {
-        this.state.socket.write(buffer);
+    if (self._reconnectTimeoutSecs > 0) {
+        debug(util.format('Reconnect after %d second(s)', self._reconnectTimeoutSecs));
+        if (this._reconnectTimer === null) {
+            this._reconnectTimer = setTimeout(function () {
+                self._reconnectTimer = null;
+                self._connect();
+            }, self._reconnectTimeoutSecs * 1000)
+        }
+    }
+
+};
+
+EtherPortClient.prototype._flushTo = function (socket) {
+    if (this._socket === null) {
+        this._socket = socket;
+        this.emit('open');
+    }
+    if (this._queue.length) {
+        this._queue.forEach(function (buffer) {
+            this._socket.write(buffer);
+        }, this);
+
+        this._queue.length = 0;
     }
 };
 
+EtherPortClient.prototype._onConnectHandler = function () {
+    var self = this;
 
-module.exports = EtherPortClient;
+    return function () {
+        debug('Socket connected');
+        self._tcp.setTimeout(0);
+        self._flushTo(self._tcp);
+    }
+};
+
+EtherPortClient.prototype._onDataHandler = function () {
+    var self = this;
+
+    return function (data) {
+        debug('Message received:', data.length, 'byte(s)');
+        self.emit('data', data);
+    };
+};
+
+EtherPortClient.prototype._onErrorHandler = function () {
+    var self = this;
+
+    return function (error) {
+        debug('Error:', error);
+        if (self._socket !== null) {
+            self._socket = null;
+        }
+        self._reconnect();
+    }
+};
+
+EtherPortClient.prototype._onTimeoutHandler = function () {
+    var self = this;
+
+    return function () {
+        debug('Connection timeout');
+        if (self._socket !== null) {
+            self._socket = null;
+        }
+        self._tcp.destroy();
+        self._reconnect();
+    }
+};
+
+EtherPortClient.prototype._onCloseHandler = function () {
+    var self = this;
+
+    return function () {
+        debug('Socket closed');
+        if (self._socket !== null) {
+            self._socket = null;
+            self._reconnect();
+        }
+    }
+};
+
+EtherPortClient.prototype.write = function (buffer) {
+
+    if (this._socket === null) {
+        debug('Message queued:', buffer.length, 'byte(s)');
+        this._queue.push(buffer);
+    }
+    else {
+        debug('Message written:', buffer.length, 'byte(s)');
+        this._socket.write(buffer);
+    }
+};
+
+//
+// Local Helper Object to wait for the open event on two Serial Port objects 
+//
+
+function OpenEventObserver(clientPort, serverPort) {
+    events.EventEmitter.call(this);
+
+    this.isClientPortOpen = false;
+    this.isServerPortOpen = false;
+
+    clientPort.on('open', this._clientPortOpenHandler(this));
+    serverPort.on('open', this._serverPortOpenHandler(this));
+}
+util.inherits(OpenEventObserver, events.EventEmitter);
+
+OpenEventObserver.prototype._clientPortOpenHandler = function (self) {
+    return function() {
+        self.isClientPortOpen = true;
+        if (self.isServerPortOpen) {
+            self.emit('open');
+        }
+    }
+};
+
+OpenEventObserver.prototype._serverPortOpenHandler = function (self) {
+    return function() {
+        self.isServerPortOpen = true;
+        if (self.isClientPortOpen) {
+            self.emit('open');
+        }
+    }
+};
+
+//
+// Helper function to chain Serial Port objects
+//
+
+function chainSerialPorts(clientPort, serverPort) {
+    var observer = new OpenEventObserver(clientPort, serverPort);
+
+    function serverPortWrite(data) {
+        try {
+            debug('writing to serverPort', data);
+            if (! Buffer.isBuffer(data)) {
+                data = new Buffer(data);
+            }
+            serverPort.write(data);
+        }
+        catch (ex) {
+            debug('error reading message', ex);
+        }
+    }
+
+    function clientPortWrite(data) {
+        try {
+            debug('writing to clientPort', data);
+            if (! Buffer.isBuffer(data)) {
+                data = new Buffer(data);
+            }
+            clientPort.write(data);
+        }
+        catch (ex) {
+            debug('error reading message', ex);
+        }
+    }
+
+    observer.on('open', function (data) {
+        serverPort.on('data', function (data) {
+            clientPortWrite(data);
+        });
+
+        clientPort.on('data', function (data) {
+            serverPortWrite(data);
+        });
+    });
+}
+
+module.exports.EtherPortClient = EtherPortClient;
+module.exports.chainSerialPorts = chainSerialPorts;
